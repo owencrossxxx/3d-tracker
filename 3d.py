2e4da7f882183f143942cf77f0ed1da9871b91c4
Owen <wouyang1@gmail.com>
2021-06-12 17:10:38 +0100

    data gen

--- 3d.py
+++ 3d.py
@@ -5,6 +5,7 @@ import csv
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import datetime
 import random
 
@@ -16,12 +17,12 @@ angle = 120
 diameter = 28
 
 
-tracker1 = Tracker(gui=True,cam_index=0)
-tracker1.addColorBlob("green1", r=0, g=255, b=0, r_min=85, r_max=110, g_min=124, g_max=255, b_min=112, b_max=160)
+tracker1 = Tracker(gui=True,cam_index=2)
+tracker1.addColorBlob("green1", r=0, g=255, b=0, r_min=77, r_max=110, g_min=118, g_max=255, b_min=100, b_max=160)
 # tracker.setCroppingPoints(tl_x=20, tl_y=20, br_x=100, br_y=100) 
 tracker1.setMorphologicalOperationParameters(dilation_size=6, erosion_size=2)
 
-tracker2 = Tracker(gui=True,cam_index=4)
+tracker2 = Tracker(gui=False,cam_index=0)
 tracker2.addColorBlob("green2", r=0, g=255, b=0, r_min=100, r_max=145, g_min=170, g_max=255, b_min=122, b_max=255)
 # tracker.setCroppingPoints(tl_x=20, tl_y=20, br_x=100, br_y=100) 
 tracker2.setMorphologicalOperationParameters(dilation_size=6, erosion_size=2)
@@ -32,32 +33,34 @@ while counter <10000:
     counter += 1
     tracker1.processCamera("green1")
     tracker1.updateVisualizations()
-    point1 = np.asfarray(tracker1.getPoints("green1"))
-
-    if counter == 1:
-        point1_original_y = min(point1[:,1])[0]
-        point1_original_x = point1[np.where(min(point1[:,1]))[0],0][0]
-    
-
-    #print (point1_original_x)
-    #print(point1[0])
-    #point1_x = point1[:,0]
-    #point1_y = point1[:,1]
-    
-
-    #print(point)
 
     tracker2.processCamera("green2")
     tracker2.updateVisualizations()
-    point2 = tracker2.getPoints("green2")
-    #print(point2[0])
 
+    point1 = np.asfarray(tracker1.getPoints("green1"))
+    point2 = np.asfarray(tracker2.getPoints("green2"))
+
+    #print(point1,point2)
+    #print(point1[:,1])
+
+    
+    if counter == 1:
+        point1_original_y = np.min(point1[:,1])
+        point1_original_x = point1[np.where(np.min(point1[:,1]))[0],0][0]
+
+        point2_original_y = np.min(point2[:,1])
+        point2_original_x = point2[np.where(np.min(point2[:,1]))[0],0][0]
+    
+    point1 = point1 - [point1_original_x,point1_original_y]
+    point2 = point2 - [point2_original_x,point2_original_y]
 
     # data processing
-    
+    x = point1[:,0]
+    y = point1[:,1]
+    z = point2[:,0]*math.cos(math.radians(30))
 
     # Live 3D reconstruction
-
+    
 
 
 # filename = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M.csv")
